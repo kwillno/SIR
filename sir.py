21171be4bbd3b3872d7d56df35e3809921bf7409
Kristian Willassen <kristian@kwill.no>
2021-10-01 13:32:03 +0200

    prelim sir class

--- sir.py
+++ sir.py
@@ -1,30 +1,9 @@
 # This is the file containing the Markov-chain simulation
-import numpy as np
 
-daysInYear = 365
-individuals = 1000
+from sirClass import SIR
 
-alpha = 0.005
-beta  = 0.01
-gamma = 0.1
+sir = SIR(10,0.005,0.01,0.1)
 
-permutationMatrix = np.array([
-		[1-beta, beta, 0],
-		[0, 1-gamma, gamma],
-		[alpha, 0, 1-alpha]
-	])
+print(sir.params())
 
-X_n = np.zeros((individuals,individuals))
-
-def simulate(X_n, P, years):
-	
-	# Find the total amount of iterations that need to be completed
-	totalIterations = years*daysInYear
-
-	for i in range(totalIterations):
-		for j in range(len(X_n)):
-			pass
-
-
-print(X_n)
-
+sir.simulate(1)
---
+++ sirClass.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class SIR: 
+	def __init__(self, population, alpha, beta, gamma):
+		self.daysInYear = 365
+
+		self.population = population
+
+		self.alpha = alpha
+		self.beta = beta
+		self.gamma = gamma
+
+		self.P = np.array([
+			[1-beta, beta, 0],
+			[0, 1-gamma, gamma],
+			[alpha, 0, 1-alpha]
+		])
+
+		self.X_n = np.array([])
+
+	def updatePopulation(self, population):
+		self.population = population
+
+	def updateParams(self, alpha, beta, gamma):
+		self.alpha = alpha
+		self.beta = beta
+		self.gamma = gamma
+
+		self.P = np.array([
+			[1-beta, beta, 0],
+			[0, 1-gamma, gamma],
+			[alpha, 0, 1-alpha]
+		])
+
+	def params(self):
+		return np.array([self.alpha, self.beta, self.gamma])
+
+	def simulate(self, years):
+		# Find total amount of needed iterations
+		totalIter = int(years*self.daysInYear)
+
+		self.X_n = np.zeros((totalIter,self.population))
+
+		for i in range(totalIter):
+			print(self.X_n[i])
+
+
