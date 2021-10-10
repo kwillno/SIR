9c4a50452da7caea1ac8fa82292430bdb2d28ea9
marcattacks <89351567+marcattacks@users.noreply.github.com>
2021-10-10 15:54:11 +0200

    Create sirClass.py

--- sirClass.py
+++ sirClass.py
@@ -33,24 +33,22 @@ class SIR:
 		self.gamma = gamma
 
 		self.P = np.array([
-			[ 1-beta,    beta,       0,      0],
-			[      0, 1-gamma,   gamma,      0],
-			[  alpha,       0, 1-alpha,      0],
-			[      0,       0,       0,    1-0.01]
+			[1-beta, beta, 0],
+			[0, 1-gamma, gamma],
+			[alpha, 0, 1-alpha]
 		])
 
-	def setInitialState(self,S=1000,I=0,R=0,V=0):
+	def setInitialState(self,S=1000,I=0,R=0):
 
-		if S+I+R+V != self.population:
-			S = self.population - (I+R+V)
+		if S+I+R != self.population:
+			S = self.population - (I+R)
 
 		susceptible = np.zeros(S)
 		infected = np.ones(I)
 		recovered = np.ones(R)*2
-		vaccinated = np.ones(V)*3
 
 		self.X_n[:,:] = 0
-		self.X_n[0] = np.concatenate((susceptible,infected,recovered,vaccinated))
+		self.X_n[0] = np.concatenate((susceptible,infected,recovered))
 
 	def simulate(self):
 
@@ -79,23 +77,13 @@ class SIR:
 
 			for j in range(self.population):
 
-				if self.X_n[i-1,j] == 3:
-					# Special case if state == 3 (Vaccinated)
-					self.X_n[i,j] = 3
-
-				elif self.X_n[i-1,j] == 2:
-					# Special case if state == 2 (Recovered)
-					if np.random.random() > self.P[int(self.X_n[i-1,j]),int(self.X_n[i-1,j])]:
-						self.X_n[i,j] = 0
-					else:
-						self.X_n[i,j] = 2
-
+				if np.random.random() > self.P[int(self.X_n[i-1,j]),int(self.X_n[i-1,j])]:
+					self.X_n[i,j] = self.X_n[i-1,j] + 1
 				else:
-					# Noe special cases for states 0 and 1, (Susceptible and Infected)
-					if np.random.random() > self.P[int(self.X_n[i-1,j]),int(self.X_n[i-1,j])]:
-						self.X_n[i,j] = self.X_n[i-1,j] + 1
-					else:
-						self.X_n[i,j] = self.X_n[i-1,j]
+					self.X_n[i,j] = self.X_n[i-1,j]
+				
+				if self.X_n[i,j] == 3:
+					self.X_n[i,j] = 0
 
 
 		for i in range(self.totalDays):
@@ -108,26 +96,29 @@ class SIR:
 		plt.imshow(self.X_n.T)
 		plt.show()
 
+	def graphSIR(self):
 
-	def graphSIR(self, show=True, index=0):
+		S = np.zeros(len(self.X_n))
+		I = S.copy()
 
-		SIRV = np.zeros((4,len(self.X_n)))
-		SIRVlabel = ["Susceptible", "Infected", "Recovered", "Vaccinated"]
+		R = S.copy()
 
-		axis = np.linspace(0,len(self.X_n),len(self.X_n))
+		axis = np.linspace(0,len(S),len(S))
 
-		for i in range(len(SIRV)):
-			for j in range(len(self.X_n)):
-				SIRV[i,j] = np.count_nonzero(self.X_n[j] == i)
+		for i in range(len(S)):
+			S[i] = np.count_nonzero(self.X_n[i] == 0)
+			I[i] = np.count_nonzero(self.X_n[i] == 1)
+			R[i] = np.count_nonzero(self.X_n[i] == 2)
 
-		plt.figure("SIRV"+str(index))
+		plt.figure("SIR")
 		plt.title("SIR-plot")
-		for i in range(len(SIRV)):
-			plt.plot(axis, SIRV[i], label=SIRVlabel[i])
-		plt.ylim([0,self.population])
+		plt.plot(axis, S, label="Susceptible")
+		plt.plot(axis, I, label="Infected")
+		plt.plot(axis, R, label="Recovered")
+		plt.plot(axis, S+I+R, label="Sum")
 		plt.legend()
-		if show:
-			plt.show()
+		plt.show()
+
 
 	def countStateDays(self, v=True):
 		stateFirst = np.sum(self.X_n[int(self.totalDays/2):,0] == 0)
@@ -165,19 +156,8 @@ class SIR:
 
 		self.CI = CIs
 
-	def findMaxInfected(self):
-		maxI_n = 1
-		for j in range(len(self.X_n)):
-			I_n = np.count_nonzero(self.X_n[j] == 1)
-			if I_n > maxI_n:
-				maxI_n = I_n
-				argmaxI_n = j
 
-		return maxI_n, argmaxI_n
-
-
-
-	def findMaxInfectedCIs(self, simulations=100, v=False, states=[50,0,0]):
+	def findMaxInfected(self, simulations=100, v=False):
 
 		maxI = np.zeros(simulations)
 		argmaxI = maxI.copy()
@@ -186,10 +166,18 @@ class SIR:
 			if v and i > 0:
 				print(f"Working on {i+1} of {len(maxI)}\tMean max I: {np.mean(maxI[:i]):.2f}, Mean argmax I: {np.mean(argmaxI[:i]):2f}", end="\r")
 			# Restart simulation
-			self.setInitialState(I=states[0], R=states[1], V=states[2])
+			self.setInitialState(S=950, I=50)
 			self.simulateWithDependence()
 
-			maxI[i], argmaxI[i] = self.findMaxInfected()
+			maxI_n = 1
+			for j in range(len(self.X_n)):
+				I_n = np.count_nonzero(self.X_n[j] == 1)
+				if I_n > maxI_n:
+					maxI_n = I_n
+					argmaxI_n = j
+
+			maxI[i]    = maxI_n
+			argmaxI[i] = argmaxI_n 
 
 		print() # Removes carriage return
 		print(f"Mean max I: {np.mean(maxI)}, Mean argmax I: {np.mean(argmaxI)}")
@@ -203,5 +191,14 @@ class SIR:
 		print(f"95% CI for arg max I: \t[{CI_argmaxI[0]:.3f},\t {CI_argmaxI[1]:.3f}], diff: {np.abs(CI_argmaxI[0] - CI_argmaxI[1]):.3f}")
 		
 	
-def poisson(lam, t, N):
-		return ((lam*t)^N)/(N!))*e**(-lam*t)
+def poisson_bool(lam, t, N):
+		return np.random.random() > ((lam*t)^N)/(N!))*e**(-lam*t)
+
+def simulate_poisson_CI(lam, t, N, maxiter):
+	realizations = 0
+	for i in range(maxiter):
+		if poisson(lam, t, N):
+			realizations += 1
+	computed_probability = realizations/N
+	return comuted_probability
+
