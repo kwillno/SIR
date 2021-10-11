88bd2ed2b4ea86f4406cbaf3ceacf611f4158bc4
marcattacks <89351567+marcattacks@users.noreply.github.com>
2021-10-11 17:56:14 +0200

    Update Poisson.py

--- Poisson.py
+++ Poisson.py
@@ -1,51 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def poisson_bool(lam, t, N):
-    poisson_sum = 0
-    for i in range(N+1):
-        poisson_sum += (((lam*t)**i)/(np.math.factorial(i)))*np.e**(-lam*t)
-    return np.random.random() < 1-poisson_sum
-
-def poisson_realizations_maxiter(lam, t, N, maxiter):
-    realizations = 0
+def poisson_realizations(lam, t, N, maxiter):
+    realizations = np.zeros(maxiter)
     for i in range(maxiter):
-        if poisson_bool(lam, t, N):
-            realizations += 1
-    computed_probability = realizations/maxiter
-    return computed_probability
+        realizations[i] = np.random.poisson(lam*t, 1) 
+    computed_probability = np.sum(realizations > 100)/len(realizations)
+    return computed_probability, realizations         
 
-def simulate_poisson_t(lam, t, N):
-    realizations = 0
-    realization_arr = np.zeros(t)
-    for i in range(t):
-        if poisson_bool(lam, t, N):
-            realizations += 1
-            realization_arr[i] = realizations
-        else:
-            realization_arr[i] = realizations
-    return realization_arr
-    
-def plot_poisson(lam, t, N):
-    x_vals = np.linspace(0, 59, 59)
-    real1 = simulate_poisson_t(lam, t, N)
-    real2 = simulate_poisson_t(lam, t, N)
-    real3 = simulate_poisson_t(lam, t, N)
-    real4 = simulate_poisson_t(lam, t, N) 
-    real5 = simulate_poisson_t(lam, t, N)
-    real6 = simulate_poisson_t(lam, t, N)
-    real7 = simulate_poisson_t(lam, t, N)
-    real8 = simulate_poisson_t(lam, t, N)
-    real9 = simulate_poisson_t(lam, t, N)
-    real10 = simulate_poisson_t(lam, t, N)
-    plt.plot(x_vals,real1)
-    plt.plot(x_vals,real2)
-    plt.plot(x_vals,real3)
-    plt.plot(x_vals,real4)
-    plt.plot(x_vals,real5)
-    plt.plot(x_vals,real6)
-    plt.plot(x_vals,real7)
-    plt.plot(x_vals,real8)
-    plt.plot(x_vals,real9)
-    plt.plot(x_vals,real10)
-    plt.show()
+
+# problem 2a***
+
+def make_exp_t_arr(lam):
+    t_vals = np.array([])
+    while np.sum(t_vals)<59:
+        val = np.random.exponential(lam, 1)[0]
+        t_vals = np.append(t_vals,val)
+    return t_vals
+
+def simulate_poisson(lam, t, N, simulation_n=10):
+    for i in range(simulation_n):
+        y_val = 0
+        #a, poisson_vals = poisson_realizations(lam, t, N, )
+        exp_t = make_exp_t_arr(lam)
+        y_val = np.random.poisson(lam*0, 1)
+        for j in range(len(exp_t)):
+            plt.plot(make_exp_t_arr[j], y_val, '_', 'hline')
+            y_val = np.random.poisson(lam*(i+1), 1)
+        plt.show()
