4045dba359c283e7c507e76d6bf6f6cf5cf6ab7f
marcattacks <89351567+marcattacks@users.noreply.github.com>
2021-10-12 16:01:26 +0200

    Update Poisson.py

--- Poisson.py
+++ Poisson.py
@@ -53,3 +53,51 @@ def simulate_poisson(lam, t, N, simulation_n=10):
     plt.ylabel("#claims")
     plt.grid()
     plt.show()
+    
+# PROBLEM 2B
+    
+    def claim_amount_P(t, gamma, lam):
+    C = 0
+    X = np.random.poisson(lam*t)
+    for j in range(X):
+        C += np.random.exponential(1/gamma)
+    return C
+
+def claim_amount_arr(t, gamma, lam):
+    X = np.random.poisson(lam*t)
+    C = np.zeros(X)
+    for j in range(1, X):
+        C[j] = C[j-1] + np.random.exponential(1/gamma)
+    return C
+
+def total_claim_amount_P(t, gamma, lam, N):
+    C = np.array([])
+    for i in range(N):
+        claim = claim_amount(t, gamma, lam)
+        C = np.append(C, claim)
+    claims_over_8mill = 0
+    for elem in C:
+        if elem>8:
+            claims_over_8mill += 1
+    P = claims_over_8mill/len(C)
+    return P
+
+def plot_claim_amounts(t, gamma, lam):
+    print(f'The amount of claims exceeding 8 million: {total_claim_amount_P(t, gamma, lam, 1000)}')
+    colors = ["c","m","y","k","r","g","b", "mediumspringgreen", "sandybrown", "mistyrose"]
+    for i in range(10):
+        exp_t = make_exp_t_arr(lam)
+        exp_t_copy = exp_t.copy()
+        y_val = claim_amount_arr(len(exp_t), gamma, lam)
+        for k in range(len(exp_t)):
+            if k>0:
+                exp_t[k] += exp_t[k-1]
+                #y_val[k] = y_val[k-1] + np.random.poisson(lam*exp_t_copy[k])
+        for j in range(len(exp_t)-1):
+            plt.hlines(y_val[j], exp_t[j], exp_t[j+1], colors[i])
+            
+    plt.title("Insurance Claim amounts")
+    plt.xlabel("Days")
+    plt.ylabel("claim amount (in millions kr)")
+    plt.grid()
+    plt.show()
